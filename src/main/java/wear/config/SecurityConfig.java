78fe6746ad49bc47fa738368e9b0f4312ed18b7a
lucasmoraisnt <rm93267@fiap.com.br>
2023-09-06 22:51:30 -0300

    Deixando permissão liberada para acessar a API

--- src/main/java/wear/config/SecurityConfig.java
+++ src/main/java/wear/config/SecurityConfig.java
@@ -3,32 +3,47 @@ package wear.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
-@EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter{
+public class SecurityConfig {
     @Autowired
-    private UserDetailsService userDetailsService;
-    
+    AuthorizationFilter authorizationFilter;
+
     @Bean
-    public BCryptPasswordEncoder passwordEncoder() {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        return http
+                .authorizeHttpRequests()
+                    .requestMatchers(HttpMethod.POST, "/api/registrar").permitAll()
+                    .requestMatchers(HttpMethod.POST, "/api/login").permitAll()
+                    .requestMatchers("/v3/api-docs/**", "/swagger-ui.html", "/swagger-ui/**" ).permitAll()
+                    .anyRequest().permitAll()
+                .and()
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .formLogin().disable()
+                .headers().frameOptions().sameOrigin()
+                .and()
+                .addFilterBefore(authorizationFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception{
+        return config.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
-    }
-    
-    @Bean
-    public AuthenticationManager authenticationManagerBean() throws Exception {
-        return super.authenticationManagerBean();
-    }
-    
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
     }
 }
--- src/main/java/wear/service/TokenService.java
+++ src/main/java/wear/service/TokenService.java
@@ -1,33 +1,35 @@
 package wear.service;
 
-import java.time.Instant;
-import java.time.temporal.ChronoUnit;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
 
 import wear.models.Credencial;
 import wear.models.Token;
 import wear.models.Usuario;
 import wear.repository.UsuarioRepository;
 
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+
+@Service
 public class TokenService {
+
     @Autowired
     UsuarioRepository usuarioRepository;
 
     @Value("${jwt.secret}")
     String secret;
 
-    public Token generateToken( Credencial credencial) {
+    public Token generateToken(Credencial credencial) {
         Algorithm alg = Algorithm.HMAC256(secret);
         var token = JWT.create()
-                    .withSubject(credencial.email())
-                    .withExpiresAt(Instant.now().plus(1, ChronoUnit.HOURS))
-                    .withIssuer("Wear")
-                    .sign(alg);
+                .withSubject(credencial.email())
+                .withExpiresAt(Instant.now().plus(1, ChronoUnit.HOURS))
+                .withIssuer("WeAr")
+                .sign(alg);
 
         return new Token(token, "JWT", "Bearer");
     }
@@ -35,13 +37,14 @@ public class TokenService {
     public Usuario valideAndGetUserBy(String token) {
         Algorithm alg = Algorithm.HMAC256(secret);
         var email =  JWT.require(alg)
-            .withIssuer("Wear")
-            .build()
-            .verify(token)
-            .getSubject()
-            ;
+                .withIssuer("WeAr")
+                .build()
+                .verify(token)
+                .getSubject()
+                ;
 
         return usuarioRepository.findByEmail(email)
-                    .orElseThrow(() -> new RuntimeException("Usuário não encontrado"));
+                .orElseThrow(() -> new RuntimeException("Usuário não encontrado"));
     }
+
 }
